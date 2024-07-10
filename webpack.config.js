8fc0fd9a667f4309d0f0254dbfbd28cc3bd1b242
ngurevich <ngurevich@outbrain.com>
2024-07-11 01:05:53 +0300

    Working on refactor to css

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,55 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'development',
-  devtool: 'inline-source-map',
+  entry: ["./src/index"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.ts$/,
         exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 50 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[hash][name][ext]",
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
+  plugins: [new Dotenv(), new HtmlWebpackPlugin({ template: "./index.html" })],
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new Dotenv()
-  ],
+  devtool: isProduction ? "none" : "inline-source-map",
+  mode: isProduction ? "production" : "development",
   devServer: {
-    static: {
-      directory: path.join(__dirname, '/'),
-    },
-    compress: true,
-    port: 9000,
-    hot: true,
+    watchFiles: ["src/**/*"],
+    static: "./dist",
   },
 };
