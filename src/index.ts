c83a132455d138d22ece834e66322f68dd7d1f21
Nati Gurevich <nati_g4@hotmail.com>
2024-07-10 15:05:20 +0300

    Added webpack, keys and html example

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const apiKey = process.env.API_KEY;
+const apiUrl = process.env.API_URL;
+
+console.log(apiKey, apiUrl);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
